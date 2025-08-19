def26feca9ad4a7f2070bbf7b488d7e0b550f316
Natanzin <natan.silva.judo@gmail.com>
2025-08-19 12:06:53 -0300

    melhoria no controle do tamanho das fontes

---
+++ service.js
@@ -0,0 +1,218 @@
+function trocaImagem() {
+    const img = document.querySelector(".a4-page");
+    const evento = document.querySelector('input[name="evento"]:checked').value;
+    const type = document.querySelector('input[name="type"]:checked').value;
+    const typeBig = document.getElementById('big')
+    const txtPequeno = document.getElementById('prismaPequeno')
+    if (evento == 'sudeco') {
+        document.getElementById('organ').disabled = true
+        document.getElementById('organ').value = ""
+        document.getElementById('fontOrgan').disabled = true
+        txtPequeno.style.textDecoration = 'none'
+        if (type === "small") {
+            img.style.backgroundImage = "url('./assets/papel/small.png')"; // Fundo prisma pequeno
+            img.style.width = '210mm'
+            img.style.height = '296mm'
+        } else {
+            img.style.backgroundImage = "url('./assets/papel/big.jpeg')"; // Fundo prisma grande
+            img.style.width = '296mm'
+            img.style.height = '210mm'
+        }
+    } else {
+        document.getElementById('organ').disabled = false
+        document.getElementById('fontOrgan').disabled = false
+        img.style.backgroundImage = "url('./assets/papel/condel.png')"; // Fundo prisma grande
+        img.style.width = '296mm'
+        img.style.height = '210mm'
+        typeBig.checked = true
+        txtPequeno.style.textDecoration = 'line-through'
+    }
+}
+
+function montaPrisma() {
+    //formulário
+    const name = document.getElementById('name').value;
+    const organ = document.getElementById('organ').value;
+    const position = document.getElementById('position').value;
+    const type = document.querySelector('input[name="type"]:checked').value;
+    const evento = document.querySelector('input[name="evento"]:checked').value;
+    const fontPosition = document.querySelector('#fontePosition').value
+    const fontOrgan = document.querySelector('#fontOrgan').value
+
+    console.log(fontPosition)
+
+    //elementos da página
+    const page = document.getElementById('a4-page')
+    const name1 = document.getElementById('name1');
+    const position1 = document.getElementById('position1');
+    const name2 = document.getElementById('name2');
+    const position2 = document.getElementById('position2');
+    //const spaceTop = document.getElementById('space-top');
+    const spaceCenter = document.getElementById('space-center');
+    const organ1 = document.getElementById('organ1');
+    const organ2 = document.getElementById('organ2');
+
+    switch (fontPosition) {
+        case "5":
+            position1.style.fontSize = "5px";
+            position2.style.fontSize = "5px";
+            break;
+        case "10":
+            position1.style.fontSize = "10px";
+            position2.style.fontSize = "10px";
+            break;
+        case "15":
+            position1.style.fontSize = "15px";
+            position2.style.fontSize = "15px";
+            break;
+        case "20":
+            position1.style.fontSize = "20px";
+            position2.style.fontSize = "20px";
+            break;
+        case "25":
+            position1.style.fontSize = "25px";
+            position2.style.fontSize = "25px";
+            break;
+        case "30":
+            position1.style.fontSize = "30px";
+            position2.style.fontSize = "30px";
+            break;
+        case "35":
+            position1.style.fontSize = "35px";
+            position2.style.fontSize = "35px";
+            break;
+        case "40":
+            position1.style.fontSize = "40px";
+            position2.style.fontSize = "40px";
+            break;
+        case "45":
+            position1.style.fontSize = "45px";
+            position2.style.fontSize = "45px";
+            break;
+        default:
+            // fallback (se der algum valor inesperado → aplica 40px)
+            position1.style.fontSize = "40px";
+            position2.style.fontSize = "40px";
+    }
+
+    switch (fontOrgan) {
+        case "5":
+            organ1.style.fontSize = "5px";
+            organ2.style.fontSize = "5px";
+            break;
+        case "10":
+            organ1.style.fontSize = "10px";
+            organ2.style.fontSize = "10px";
+            break;
+        case "15":
+            organ1.style.fontSize = "15px";
+            organ2.style.fontSize = "15px";
+            break;
+        case "20":
+            organ1.style.fontSize = "20px";
+            organ2.style.fontSize = "20px";
+            break;
+        case "25":
+            organ1.style.fontSize = "25px";
+            organ2.style.fontSize = "25px";
+            break;
+        case "30":
+            organ1.style.fontSize = "30px";
+            organ2.style.fontSize = "30px";
+            break;
+        case "35":
+            organ1.style.fontSize = "35px";
+            organ2.style.fontSize = "35px";
+            break;
+        case "40":
+            organ1.style.fontSize = "40px";
+            organ2.style.fontSize = "40px";
+            break;
+        case "45":
+            organ1.style.fontSize = "45px";
+            organ2.style.fontSize = "45px";
+            break;
+        default:
+            // fallback (se der algum valor inesperado → aplica 40px)
+            organ1.style.fontSize = "30px";
+            organ2.style.fontSize = "30px";
+    }
+
+    //monta o prisma
+    if (type === 'big') {
+        //limpa todos os campos
+        name1.innerText = ''
+        position1.innerText = ''
+        organ1.innerText = ''
+        name2.innerText = ''
+        position2.innerText = ''
+        organ2.innerText = ''
+
+        // Prisma grande → texto na vertical, espelhado na horizontal
+        name1.innerText = name
+        position1.innerText = position
+        name1.style.transform = 'rotate(180deg)'
+        position1.style.transform = 'rotate(180deg)'
+        position2.innerText = position
+        name2.innerText = name
+        spaceCenter.style.height = '330px'
+        position1.style.margin = '0 60px'
+        position2.style.margin = '0 60px'
+        
+        if (evento == 'condel') {
+            spaceCenter.style.height = '280px'
+            position1.style.margin = '0 80px'
+            position2.style.margin = '0 80px'
+            if (organ != '') {
+                organ1.innerText = organ
+                organ1.style.transform = 'rotate(180deg)'
+                organ2.innerText = organ
+                spaceCenter.style.height = '240px'
+            }
+        }
+    } else {
+        // Prisma pequeno → texto na horizontal, espelhado na vertical
+        name1.innerText = name
+        position1.innerText = position
+        name1.style.transform = 'rotate(180deg)'
+        position1.style.transform = 'rotate(180deg)'
+        position2.innerText = position
+        name2.innerText = name
+        spaceCenter.style.height = '340px'
+        position1.style.margin = '0 55px'
+        position2.style.margin = '0 55px'
+    }
+}
+
+function generatePrisma() {
+    montaPrisma()
+
+    const type = document.querySelector('input[name="type"]:checked').value;
+    const name = document.getElementById('name').value;
+    const evento = document.querySelector('input[name="evento"]:checked').value;
+
+    let orientation
+    let nameFile
+
+    if (type == 'big') {
+        orientation = 'landscape'
+        if (evento == 'condel') {
+            nameFile = 'prisma grande condel - ' + name + '.pdf'
+        } else if (evento == 'sudeco') {
+            nameFile = 'prisma grande sudeco - ' + name + '.pdf'
+        }
+    } else {
+        orientation = 'portrait'
+        nameFile = 'prisma pequeno - ' + name + '.pdf'
+    }
+
+    const element = document.getElementById('a4Content');
+    const opt = {
+        margin: 0,
+        filename: nameFile,
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2, scrollX: 0, scrollY: 0, useCORS: true },
+        jsPDF: { unit: 'mm', format: 'a4', orientation: orientation }
+    };
+    html2pdf().set(opt).from(element).save();
+}
